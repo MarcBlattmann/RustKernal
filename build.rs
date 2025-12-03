32df06ce49fa7b6b5df90d4e2a0dbb4c42fc8236
Marc Blattmann <marc.blattmann08@gmail.com>
2025-12-03 21:34:18 +0100

    Disable frame buffer logging in boot images
    Set BootConfig.frame_buffer_logging to false for both BIOS and UEFI disk images in build.rs to disable yellow log output during boot.

--- build.rs
+++ build.rs
@@ -1,18 +1,24 @@
 use std::path::PathBuf;
+use bootloader::BootConfig;
 
 fn main() {
     let out_dir = PathBuf::from(std::env::var_os("OUT_DIR").unwrap());
     let kernel = PathBuf::from(std::env::var_os("CARGO_BIN_FILE_KERNEL_kernel").unwrap());
 
+    // Configure boot settings - disable the yellow log output
+    let mut boot_config = BootConfig::default();
+    boot_config.frame_buffer_logging = false;
     // Create BIOS disk image
     let bios_path = out_dir.join("bios.img");
     bootloader::BiosBoot::new(&kernel)
+        .set_boot_config(&boot_config)
         .create_disk_image(&bios_path)
         .unwrap();
 
     // Create UEFI disk image (optional)
     let uefi_path = out_dir.join("uefi.img");
     bootloader::UefiBoot::new(&kernel)
+        .set_boot_config(&boot_config)
         .create_disk_image(&uefi_path)
         .unwrap();
 
