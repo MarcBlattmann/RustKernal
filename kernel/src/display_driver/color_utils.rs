088e5b514fd237a28e1a1f98df157de41d8bdf01
Marc Blattmann <marc.blattmann08@gmail.com>
2025-12-04 20:42:43 +0100

    Refactor color_to_pixel_bytes to return Option
    The function now returns an Option<[u8; 4]> instead of always returning a pixel array. This change improves error handling by returning None for invalid hex lengths, making the function's behavior clearer and safer.

--- kernel/src/display_driver/color_utils.rs
+++ kernel/src/display_driver/color_utils.rs
@@ -1,38 +1,29 @@
 use bootloader_api::info::PixelFormat;
 use crate::tools::digit_count;
 
-pub fn color_to_pixel_bytes(hex: u32, format: PixelFormat) -> [u8; 4] {
-    let mut alpha = 255;
-    let mut red = 0;
-    let mut green = 0;
-    let mut blue = 0;
-
-    if digit_count(hex as u64) == 6  {
-        red = ((hex >> 16) & 0xFF) as u8;
-        green = ((hex >> 8) & 0xFF) as u8;
-        blue = (hex & 0xFF) as u8;
-    } else if digit_count(hex as u64) == 8 {
-        alpha = ((hex >> 24) & 0xFF) as u8;
-        red = ((hex >> 16) & 0xFF) as u8;
-        green = ((hex >> 8) & 0xFF) as u8;
-        blue = (hex & 0xFF) as u8;
-    } else {
-        return [red, green, blue, alpha];
+pub fn color_to_pixel_bytes(hex: u32, format: PixelFormat) -> Option<[u8; 4]> {
+    let hex_length = digit_count(hex as u64);
+    if hex_length != 6 && hex_length != 8  {
+        return None;
     }
 
+    let red = ((hex >> 16) & 0xFF) as u8;
+    let green = ((hex >> 8) & 0xFF) as u8;
+    let blue = (hex & 0xFF) as u8;
+
     match format {
-        PixelFormat::Rgb => [red, green, blue, 0],
-        PixelFormat::Bgr => [blue, green, red, 0],
+        PixelFormat::Rgb => Some([red, green, blue, 0]),
+        PixelFormat::Bgr => Some([blue, green, red, 0]),
         PixelFormat::U8 => {
             let gray = ((red as u16 + green as u16 + blue as u16) / 3) as u8;
-            [gray, 0, 0, 0]
+            return Some([gray, 0, 0, 0]);
         },
         PixelFormat::Unknown { red_position, green_position, blue_position } => {
             let r = ((hex >> red_position) & 0xFF) as u8;
             let g = ((hex >> green_position) & 0xFF) as u8;
             let b = ((hex >> blue_position) & 0xFF) as u8;
-            return [r, g, b, 0];
+            return Some([r, g, b, 0]);
         },
-        _ => [red, green, blue, 0],
+        _ => Some([red, green, blue, 0]),
     }
 }
