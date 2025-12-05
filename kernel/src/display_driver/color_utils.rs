6fe1dffd3f96dc83c302ca9bb2b8f4fc50d9cd16
Marc <marc.blattmann08@gmail.com>
2025-12-05 10:49:04 +0100

    Simplify color component extraction in color_to_bytes
    Removed unnecessary masking when extracting red, green, and blue components from the hex color value, as casting to u8 already truncates higher bits.

--- kernel/src/display_driver/color_utils.rs
+++ kernel/src/display_driver/color_utils.rs
@@ -2,9 +2,9 @@ use bootloader_api::info::PixelFormat;
 
 pub fn color_to_bytes(hex: u32, format: PixelFormat) -> Option<[u8; 4]> {
 
-    let red = ((hex >> 16) & 0xFF) as u8;
-    let green = ((hex >> 8) & 0xFF) as u8;
-    let blue = (hex & 0xFF) as u8;
+    let red = (hex >> 16) as u8;
+    let green = (hex >> 8) as u8;
+    let blue = (hex) as u8;
 
     match format {
         PixelFormat::Rgb => Some([red, green, blue, 0]),
