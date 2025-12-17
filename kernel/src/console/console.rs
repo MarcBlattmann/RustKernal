4497b2f14c931768bc79f6cc938528b5facb023f
Marc <marc.blattmann08@gmail.com>
2025-12-17 09:11:28 +0100

    Add Console::new constructor
    Introduced a new associated function `Console::new` to create a Console instance from a Screen. This improves initialization and encapsulation of the Console struct.

--- kernel/src/console/console.rs
+++ kernel/src/console/console.rs
@@ -3,3 +3,10 @@ use crate::display_driver::display::Screen;
 pub struct Console {
     pub screen: Screen,
 }
+
+
+impl Console {
+    pub fn new(screen: Screen) -> Self {
+        Console { screen }
+    }
+}
