d53908e04b9f771bbce331024a3839f7a9cd9c18
Marc Blattmann <marc.blattmann08@gmail.com>
2026-01-10 16:30:26 +0100

    Remove divide by zero handler from IDT
    The divide by zero exception handler and its registration in the IDT have been removed. This simplifies the interrupt descriptor table setup and exception handling.

--- kernel/src/idt.rs
+++ kernel/src/idt.rs
@@ -11,17 +11,11 @@ static IDT: Lazy<InterruptDescriptorTable> = Lazy::new(|| {
             .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
     }
 
-    idt.divide_error.set_handler_fn(divide_by_zero_handler);
-    
     return idt;
 });
 
 extern "x86-interrupt" fn double_fault_handler(stack_frame: InterruptStackFrame, error_code: u64) -> ! {
     panic!("EXCEPTION: DOUBLE FAULT (error code: {})\n{:#?}", error_code, stack_frame);
-}
-
-extern "x86-interrupt" fn divide_by_zero_handler(stack_frame: InterruptStackFrame) {
-    panic!("EXCEPTION: DIVIDE BY ZERO\n{:#?}", stack_frame);
 }
 
 pub fn init() {
